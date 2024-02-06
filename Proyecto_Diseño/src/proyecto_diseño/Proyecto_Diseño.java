a2962ea562e9ccb19c3d5f7f5ced0d9e0e977350
AlejandroBel27 <ruben.saldana216100@potros.itson.edu.mx>
2024-02-06 16:58:52 -0700

    first commit

---
+++ Proyecto_Diseño/src/proyecto_diseño/Proyecto_Diseño.java
@@ -0,0 +1,16 @@
+package proyecto_diseño;
+
+/**
+ *
+ * @author Linda-Ruben-Max-David
+ */
+public class Proyecto_Diseño {
+
+    /**
+     * @param 
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
