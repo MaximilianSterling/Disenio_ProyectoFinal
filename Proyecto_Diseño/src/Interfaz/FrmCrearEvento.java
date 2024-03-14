426a8bcdc1f076fdbdb83f1a73fd3d77a0c9bae4
DavidElierCampaChaparro <david.campa245178@potros.itson.edu.mx>
2024-03-14 01:11:28 -0700

    Reenvio del commit con el FrmCrearEvento y FrmVisualizarEvento

--- Proyecto_Diseño/src/Interfaz/FrmCrearEvento.java
+++ Proyecto_Diseño/src/Interfaz/FrmCrearEvento.java
@@ -147,6 +147,7 @@ public class FrmCrearEvento extends javax.swing.JFrame {
         );
 
         pack();
+        setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
 
     private void campoTextoEventoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_campoTextoEventoActionPerformed
