ea483ffba9757e5715f7de50bbe68d65aeea1f8b
linmoreno <linda.moreno216690@potros.itson.edu.mx>
2024-04-14 17:48:54 -0700

    Programación de entidades, DAO, conexion a BD

--- Eventos_Potros/src/main/java/DAO/EventosDAO.java
+++ Eventos_Potros/src/main/java/DAO/EventosDAO.java
@@ -4,10 +4,30 @@
  */
 package DAO;
 
+import ConexionMySQL.conexionMySQL;
+import entidades.evento;
+import javax.persistence.EntityManager;
+
 /**
  *
  * @author cocob
  */
 public class EventosDAO {
-    
+
+    public void insertarEvento(evento evento) {
+        EntityManager em = null;
+        try {
+            em = conexionMySQL.createEntityManager();
+            em.getTransaction().begin();
+            em.persist(evento);
+            em.getTransaction().commit();
+            System.out.println("Guardado correctamente");
+        } catch (Exception e) {
+            System.out.println("Error al guardar: " + e.getMessage());
+        } finally {
+            if (em != null) {
+                em.close();
+            }
+        }
+    }
 }
--- Eventos_Potros/src/main/java/entidades/evento.java
+++ Eventos_Potros/src/main/java/entidades/evento.java
@@ -17,7 +17,7 @@ public class evento {
 
     @Id //Identificacion del id
     @Column(name = "idEvento") //Identificacion de la columna
-    @GeneratedValue(strategy = GenerationType.AUTO) //Identifica que la variable es autoincrementable
+    @GeneratedValue(strategy = GenerationType.SEQUENCE) //Identifica que la variable es autoincrementable
     private int idEvento;
     @Column(name = "nombre")
     private String nombre;
